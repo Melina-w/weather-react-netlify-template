df5777c3b92ead3b1768efe79e3524bb112ce63c
MELINA <meluwaigant@gmail.com>
2020-12-28 20:06:53 +1300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Melina Waigant and it is {""}
+        <a
+          href="https://github.com/Melina-w/weather-react-netlify-template"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
