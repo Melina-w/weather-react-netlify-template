765f064b2d1dc4d43ca1d2176bca78a7ec11a946
MELINA <meluwaigant@gmail.com>
2020-12-28 20:26:48 +1300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Melina Waigant and it is {""}
-        <a
-          href="https://github.com/Melina-w/weather-react-netlify-template"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Melina Waigant and it is {""}
+          <a
+            href="https://github.com/Melina-w/weather-react-netlify-template"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
